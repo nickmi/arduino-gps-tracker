3abf976525e52b619b8602bebe121c8a22241907
nickmi <nikolasmichalaros@gmail.com>
2018-05-13 19:40:38 +0300

    orgs ch

--- main.c
+++ main.c
@@ -67,34 +67,34 @@ void setup() {
 void loop() {
 
 
-		if(getLocation()) { //If getLocation gets TRUE, print the rest!
-				//Print Lat/Lon Values
-				mqtt.loop();
-				mqqtConnectionEngine();
-				mqtt.loop();
-				SerialMon.print(Lat);
-				SerialMon.print(" : ");
-				SerialMon.print(Lon);
-				SerialMon.print(" at ");
-				SerialMon.print(Time);
-				SerialMon.print(" ");
-				SerialMon.print(Date);
-				SerialMon.print("\n");
+	if(getLocation()) { //If getLocation gets TRUE, print the rest!
+		//Print Lat/Lon Values
+		mqtt.loop();
+		mqqtConnectionEngine();
+		mqtt.loop();
+		SerialMon.print(Lat);
+		SerialMon.print(" : ");
+		SerialMon.print(Lon);
+		SerialMon.print(" at ");
+		SerialMon.print(Time);
+		SerialMon.print(" ");
+		SerialMon.print(Date);
+		SerialMon.print("\n");
 
-				flushFONA();
-				SerialMon.print("End of getLocation\n");
-				for(int i = 1; i < SLEEP_MINUTES; i++) {
-						delay(30000);
-						SerialMon.print("Minute:");
-						SerialMon.print(i);
-						SerialMon.print("\n");
-				}
-				SerialMon.print("inizializing..wait 10 seconds..\n");
-				delay(10000);
-
-		} else {
-				delay(5000);
+		flushFONA();
+		SerialMon.print("End of getLocation\n");
+		for(int i = 1; i < SLEEP_MINUTES; i++) {
+			delay(30000);
+			SerialMon.print("Minute:");
+			SerialMon.print(i);
+			SerialMon.print("\n");
 		}
+		SerialMon.print("inizializing..wait 10 seconds..\n");
+		delay(10000);
+
+	} else {
+		delay(5000);
+	}
 }
 
 boolean mqttConnect() {
@@ -106,34 +106,34 @@ boolean mqttConnect() {
 	}
 	SerialMon.println(" OK");
 
-return mqtt.connected();
+	return mqtt.connected();
 
 }
 
 void publishPayLoad(){
 
- SerialMon.print("Sending payload: ");
+	SerialMon.print("Sending payload: ");
 
- if (mqtt.publish(topic, (char*) buildJson().c_str())) {
-	 SerialMon.println("Publish ok");
- } else {
-	 SerialMon.println("Publish failed");
- }
+	if (mqtt.publish(topic, (char*) buildJson().c_str())) {
+		SerialMon.println("Publish ok");
+	} else {
+		SerialMon.println("Publish failed");
+	}
 }
 
 String buildJson() {
 
-const size_t bufferSize = JSON_ARRAY_SIZE(2);
-DynamicJsonBuffer jsonBuffer(bufferSize);
+	const size_t bufferSize = JSON_ARRAY_SIZE(2);
+	DynamicJsonBuffer jsonBuffer(bufferSize);
 
-JsonObject& root = jsonBuffer.createObject();
+	JsonObject& root = jsonBuffer.createObject();
 
-root["Latitude"]=Lat;
-root["Longitude"]=Lon;
-String jsonStr;
-root.printTo(jsonStr);
-	 return jsonStr;
-	}
+	root["Latitude"]=Lat;
+	root["Longitude"]=Lon;
+	String jsonStr;
+	root.printTo(jsonStr);
+	return jsonStr;
+}
 
 void mqqtConnectionEngine(){
 
@@ -153,7 +153,7 @@ void mqqtConnectionEngine(){
 			}
 		}
 	}
-	}
+}
 
 void setupInternet(){
 
@@ -178,101 +178,101 @@ void setupInternet(){
 		while (true);
 	}
 	SerialMon.println(" OK");
-	}
+}
 
 //GPS FUNCTIONS
 void setupgps(){
 	String ans;
 
 	SerialMon.print("get localization ");
-		if(sendATCommand("AT+CGNSSEQ=RMC", ans)){ //setup for GPS for fona 808
-				SerialMon.print(ans + "\n");
+	if(sendATCommand("AT+CGNSSEQ=RMC", ans)){ //setup for GPS for fona 808
+		SerialMon.print(ans + "\n");
 	}
 }
 
 boolean getLocation() { //all the commands to get location from gps
-		//the sendATCommand sends the command to the FONA and waits until the recieves a response before continueing on.
-		String ans;
+	//the sendATCommand sends the command to the FONA and waits until the recieves a response before continueing on.
+	String ans;
 
-		SerialMon.print("get localization ");
-		if(sendATCommand("AT+CGNSINF", ans)){ //turns off GPS for fona 808
-				SerialMon.print(ans + "\n");
+	SerialMon.print("get localization ");
+	if(sendATCommand("AT+CGNSINF", ans)){ //turns off GPS for fona 808
+		SerialMon.print(ans + "\n");
 
-			if(ans.startsWith("+CGNSINF: 1,1,")) {
-						SerialMon.print("Got Location\n"); //+CGNSINF: 1,1,20161222121541.000,41.931233,2.245903,530.100,0.04,269.5,1,,1.0,
-						Date = ans.substring(14, 22);
-						SerialMon.print("Date:" + Date + "\n");
-						Time = ans.substring(22,28);
-						SerialMon.print("Time:" + Time + "\n");
-						Lat = ans.substring(33, 42);
-						SerialMon.print("Lat:" + Lat + "\n");
-						Lon = ans.substring(43, 51);
-						SerialMon.print("Lon:" + Lon + "\n");
-						return 1;
+		if(ans.startsWith("+CGNSINF: 1,1,")) {
+			SerialMon.print("Got Location\n"); //+CGNSINF: 1,1,20161222121541.000,41.931233,2.245903,530.100,0.04,269.5,1,,1.0,
+			Date = ans.substring(14, 22);
+			SerialMon.print("Date:" + Date + "\n");
+			Time = ans.substring(22,28);
+			SerialMon.print("Time:" + Time + "\n");
+			Lat = ans.substring(33, 42);
+			SerialMon.print("Lat:" + Lat + "\n");
+			Lon = ans.substring(43, 51);
+			SerialMon.print("Lon:" + Lon + "\n");
+			return 1;
 
-				} else {  //If the response of the device is does not start with +CGNSINF: 1,1,... then do nothing
-						SerialMon.print("------------------debugger start-----------------\n");
-						SerialMon.print("LOCATION NOT YET ACQUIRED \n");
-						SerialMon.print(ans + "\n");
-						SerialMon.print("------------------debugger stop-----------------\n");
+		} else {  //If the response of the device is does not start with +CGNSINF: 1,1,... then do nothing
+			SerialMon.print("------------------debugger start-----------------\n");
+			SerialMon.print("LOCATION NOT YET ACQUIRED \n");
+			SerialMon.print(ans + "\n");
+			SerialMon.print("------------------debugger stop-----------------\n");
 
-						return 0;
-				}
-		} else { // if sendATCommand failed
-			SerialMon.print("AT command failed: " + ans + "\n");
 			return 0;
 		}
+	} else { // if sendATCommand failed
+		SerialMon.print("AT command failed: " + ans + "\n");
+		return 0;
+	}
 
 }
 
 void GPSon() { //Turn on GPS
-		//the sendATCommand sends the command to the FONA and waits until the recieves a response before continueing on.
-		String ans;
+	//the sendATCommand sends the command to the FONA and waits until the recieves a response before continueing on.
+	String ans;
 
-		SerialMon.print("TRYING TO TURN ON GPS ");
-		if(sendATCommand("AT+CGNSPWR=1", ans)){ //Power GPS (1 - ON, 0 - OFF)
-				SerialMon.print(ans + "\n");
-		}
+	SerialMon.print("TRYING TO TURN ON GPS ");
+	if(sendATCommand("AT+CGNSPWR=1", ans)){ //Power GPS (1 - ON, 0 - OFF)
+		SerialMon.print(ans + "\n");
+	}
 }
 
 void GPSoff() { //all the commands to setup a GPS
-		//the sendATCommand sends the command to the FONA and waits until the recieves a response before continueing on.
-		String ans;
+	//the sendATCommand sends the command to the FONA and waits until the recieves a response before continueing on.
+	String ans;
 
-		SerialMon.print("Turn off GPS ");
-		if(sendATCommand("AT+CGNSPWR=0", ans)){ //Power GPS (1 - ON, 0 - OFF)
-				SerialMon.print(ans + "\n");
-		}
+	SerialMon.print("Turn off GPS ");
+	if(sendATCommand("AT+CGNSPWR=0", ans)){ //Power GPS (1 - ON, 0 - OFF)
+		SerialMon.print(ans + "\n");
+	}
 }
 
 boolean sendATCommand(String Command, String& ans) { //Send an AT command and wait for a response
-		int complete = 0; // have we collected the whole response?
-		char c; //capture serial stream
-		String content; //place to save serial stream
-		unsigned long commandClock = millis(); //timeout Clock
+	int complete = 0; // have we collected the whole response?
+	char c; //capture serial stream
+	String content; //place to save serial stream
+	unsigned long commandClock = millis(); //timeout Clock
 
-		ans = "";
-		SerialAT.println(Command); //Print Command
-		while(!complete && commandClock <= millis() + ATtimeOut) { //wait until the command is complete
-				while(!SerialAT.available() && commandClock <= millis()+ATtimeOut); //wait until the Serial Port is opened
-				while(SerialAT.available()) { //Collect the response
-						c = SerialAT.read(); //capture it
-						if(c == 0x0A || c == 0x0D); //disregard all new lines and carrige returns (makes the String matching eaiser to do)
-						else content.concat(c); //concatonate the stream into a String
-				}
-				Serial.print("command: '" + String(Command) + "', response: '" + content + "'\n"); //Debug
-				ans = content; //save the received string(content) to "ans"
-				complete = 1;  //Lable as Done.
+	ans = "";
+	SerialAT.println(Command); //Print Command
+	while(!complete && commandClock <= millis() + ATtimeOut) { //wait until the command is complete
+		while(!SerialAT.available() && commandClock <= millis()+ATtimeOut); //wait until the Serial Port is opened
+		while(SerialAT.available()) { //Collect the response
+			c = SerialAT.read(); //capture it
+			if(c == 0x0A || c == 0x0D); //disregard all new lines and carrige returns (makes the String matching eaiser to do)
+			else content.concat(c); //concatonate the stream into a String
 		}
-		if (complete ==1) return 1; //Is it done? return a 1
-		else return 0; //otherwise don't (this will trigger if the command times out)
+		Serial.print("command: '" + String(Command) + "', response: '" + content + "'\n"); //Debug
+		ans = content; //save the received string(content) to "ans"
+		complete = 1;  //Lable as Done.
+	}
+	if (complete ==1) return 1; //Is it done? return a 1
+	else return 0; //otherwise don't (this will trigger if the command times out)
 
 }
 void flushFONA() { //if there is anything is the SerialAT serial Buffer, clear it out and print it in the Serial Monitor.
-		char inChar;
-		while (SerialAT.available()){
-				inChar = SerialAT.read();
-				Serial.write(inChar);
-				delay(20);
-		}
+	char inChar;
+	while (SerialAT.available()){
+		inChar = SerialAT.read();
+		Serial.write(inChar);
+		delay(20);
+	}
 }
